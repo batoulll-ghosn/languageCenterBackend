917522300665f296920e4c9b41f1f3fb2e18db23
batoulll-ghosn <batoul.ghosn@isae.edu.lb>
2023-11-08 14:39:34 +0200

    final

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const corsOptions = {
     credentials: true,
 }
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.setHeader('Access-Control-Allow-Origin', 'https://language-learning-center1212.onrender.com');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     res.setHeader('Access-Control-Allow-Credentials', 'true');
